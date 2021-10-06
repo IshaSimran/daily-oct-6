68f88b47cea5e228921b7af7abecaefc9914d46b
Isha Sidhu <ishasimranxo@gmail.com>
2021-10-06 13:36:36 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Daily October.6.2021
+## By Isha Simran
+
+## Sources
+
+- [String](https://www.programiz.com/javascript/string)
+
+- [Number](https://www.programiz.com/javascript/numbers)
+
+- [Boolean](https://www.programiz.com/javascript/booleans)
+
+- [Null](https://www.programiz.com/javascript/null-undefined)
+
+- [Array](https://www.programiz.com/javascript/array)
+
+- [Object](https://www.programiz.com/javascript/object)
+
+- [Function](https://www.programiz.com/javascript/function)
---
+++ js/js_literals.js
@@ -0,0 +1,47 @@
+// String
+const name = 'Peter';
+const name1 = "Jack";
+const result = `The names are ${name} and ${name1}`;
+
+// Number
+const a = 3;
+const b = 3.13;
+
+// Boolean
+const a = 'true';
+console.log(typeof a);
+
+// Null
+let number = null;
+
+// Arrays
+
+// empty array
+const myList = [];
+
+// array of numbers
+const numberArray = [2, 4, 6, 8];
+
+// array of strings
+const stringArray = ['eat', 'work', 'sleep'];
+
+// array with mixed data types
+const newData = ['work', 'exercise', 1, true];
+
+// Object
+const student = {
+  firstName: 'ram',
+  class: 10
+};
+
+// Funtion
+// program to print a text
+// declaring a function
+function greet() {
+  console.log("Hello there!");
+}
+
+// calling the function
+greet();
+
+// Output is Hello There!
